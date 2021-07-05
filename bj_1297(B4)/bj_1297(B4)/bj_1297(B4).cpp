69d0a3549215a730c7ac846da925c840f48b17fd
LeeJoEun-01 <joeun1005@naver.com>
2021-07-06 02:32:53 +0900

    2021.07.06
    float보다는 double 쓰자

---
+++ bj_1297(B4)/bj_1297(B4)/bj_1297(B4).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // 백준 브론즈 4 1330번
+    double a;
+    double b;
+    cin >> a >> b;
+
+    if (a > b) {
+        cout << ">";
+    }
+    else if (a < b) {
+        cout << "<";
+    }
+    else if (a == b) {
+        cout << "==";
+    }
+}
+
+// 백준 브론즈 4 1297번
+    /*double x;
+    double h;
+    double w;
+    double y;
+
+    cin >> x >> h >> w;
+
+    y = sqrt( x*x / (h*h + w*w));
+
+    cout << int(h*y) << " " << int(w*y) ;*/
